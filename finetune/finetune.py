39b4b50ff0ac428567e2eb02ab4f8aec135bdacd
Chen Quan Lin <114123769+MrChen314@users.noreply.github.com>
2024-08-26 10:23:03 +0800

    Update finetune.py

--- finetune/finetune.py
+++ finetune/finetune.py
@@ -66,41 +66,6 @@ class LoraArguments:
     lora_layer_replication: Optional[List[Tuple[int, int]]] = None
     lora_layers_to_transform: Optional[List[int]] = None
     lora_layers_pattern: Optional[str] = None
-   
-def maybe_zero_3(param):
-    if hasattr(param, "ds_id"):
-        assert param.ds_status == ZeroParamStatus.NOT_AVAILABLE
-        with zero.GatheredParameters([param]):
-            param = param.data.detach().cpu().clone()
-    else:
-        param = param.detach().cpu().clone()
-    return param
-
-
-# Borrowed from peft.utils.get_peft_model_state_dict
-def get_peft_state_maybe_zero_3(named_params, bias):
-    if bias == "none":
-        to_return = {k: t for k, t in named_params if "lora_" in k}
-    elif bias == "all":
-        to_return = {k: t for k, t in named_params if "lora_" in k or "bias" in k}
-    elif bias == "lora_only":
-        to_return = {}
-        maybe_lora_bias = {}
-        lora_bias_names = set()
-        for k, t in named_params:
-            if "lora_" in k:
-                to_return[k] = t
-                bias_name = k.split("lora_")[0] + "bias"
-                lora_bias_names.add(bias_name)
-            elif "bias" in k:
-                maybe_lora_bias[k] = t
-        for k, t in maybe_lora_bias:
-            if bias_name in lora_bias_names:
-                to_return[bias_name] = t
-    else:
-        raise NotImplementedError
-    to_return = {k: maybe_zero_3(v) for k, v in to_return.items()}
-    return to_return
 
 
 local_rank = None
@@ -111,18 +76,8 @@ def rank0_print(*args):
 
 def safe_save_model_for_hf_trainer(trainer, output_dir: str, bias="none"):
     """Collects the state dict and dump to disk."""
-    # check if zero3 mode enabled
-    if deepspeed.is_deepspeed_zero3_enabled():
-        state_dict = trainer.model_wrapped._zero3_consolidated_16bit_state_dict()
-    else:
-        if trainer.args.use_lora:
-            state_dict = get_peft_state_maybe_zero_3(
-                trainer.model.named_parameters(), bias
-            )
-        else:
-            state_dict = trainer.model.state_dict()
     if trainer.args.should_save and trainer.args.local_rank == 0:
-        trainer._save(output_dir, state_dict=state_dict)
+        trainer.save_model(output_dir, )
 
 
 def make_supervised_data_module(
@@ -187,7 +142,7 @@ def get_parameter_number(model):
         all_param += num_params
         if param.requires_grad:
             trainable_params += num_params
-        
+
     return {'Total': all_param, 'Trainable': trainable_params}
 
 
@@ -195,6 +150,7 @@ local_rank = 0
 
 
 def train():
+    seed_all(is_gpu=False, mode=False)
     global local_rank
     parser = transformers.HfArgumentParser(
         (ModelArguments, DataArguments, TrainingArguments, LoraArguments)
@@ -207,7 +163,7 @@ def train():
         lora_args,
     ) = parser.parse_args_into_dataclasses()
 
-    if getattr(training_args, "deepspeed", None) : 
+    if getattr(training_args, "deepspeed", None):
         training_args.distributed_state.distributed_type = DistributedType.DEEPSPEED
 
     compute_dtype = (
@@ -226,7 +182,7 @@ def train():
             logging.warning(
                 "FSDP or ZeRO3 are not incompatible with QLoRA."
             )
-    
+
     model = AutoModel.from_pretrained(
         model_args.model_name_or_path,
         trust_remote_code=True,
@@ -242,14 +198,17 @@ def train():
         model.vpm.requires_grad_(False)
     if not training_args.tune_llm:
         model.llm.requires_grad_(False)
-        
+
     if training_args.use_lora:
         if training_args.use_lora and training_args.tune_llm:
             raise ValueError("The model cannot simultaneously adjust LLM parameters and apply LoRA.")
-            
+
         rank0_print("Currently using LoRA for fine-tuning the MiniCPM-V model.")
         for name, param in model.llm.named_parameters():
             param.requires_grad = False
+        modules_to_save = ['embed_tokens', 'resampler']
+        if training_args.tune_vision:
+            modules_to_save.append('vpm')
         lora_config = LoraConfig(
             r=lora_args.lora_r,
             lora_alpha=lora_args.lora_alpha,
@@ -257,7 +216,7 @@ def train():
             lora_dropout=lora_args.lora_dropout,
             bias=lora_args.lora_bias,
             layers_to_transform=lora_args.lora_layers_to_transform,
-            task_type="CAUSAL_LM",
+            modules_to_save=modules_to_save,
         )
         if not hasattr(model, 'get_input_embeddings'):
             def get_input_embeddings(self):
@@ -268,20 +227,15 @@ def train():
                 model, use_gradient_checkpointing=training_args.gradient_checkpointing
             )
         model = get_peft_model(model, lora_config)
-        model.base_model.resampler.requires_grad_(True)
-        model.base_model.llm.model.embed_tokens.weight.requires_grad_(True)
-        if training_args.tune_vision:
-            model.base_model.vpm.requires_grad_(True)
         if training_args.gradient_checkpointing:
             model.enable_input_require_grads()
 
     rank0_print(get_parameter_number(model))
 
-    llm_type = training_args.llm_type    
-    
+    llm_type = training_args.llm_type
+
     rank0_print(f'llm_type={llm_type}')
 
-    
     # Load data
     if hasattr(model.config, "slice_config"):
         model.config.slice_config.max_slice_nums = training_args.max_slice_nums
@@ -307,7 +261,7 @@ def train():
         batch_vision=batch_vision,
         max_length=training_args.model_max_length,
     )
-    
+
     trainer = CPMTrainer(
         model=model,
         tokenizer=tokenizer,
